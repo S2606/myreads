4764a7b70ceca13700ad984e688c632ce83e10d5
Shagun Khemka <shagun.khemka60@gmail.com>
2020-05-22 18:22:09 +0530

    refractor: Convert components into stateless functional components
    convert books and listbooks into stateless functional components

--- src/Book.js
+++ src/Book.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ShelfChange from './ShelfChange';
 
-class Book extends Component{
-    render() {
+const Book = props => {
+    const {book, handleStatusChange, bookShelfTypes, getCurrentBookStatus } = props;
 
-        const {book, handleStatusChange, bookShelfTypes, getCurrentBookStatus } = this.props;
-
-        return (
-            <div className="book">
-                <div className="book-top">
-                <div className="book-cover" 
-                style={{ width: 128, 
-                         height: 193, 
-                         backgroundImage: `url(${book.imageLinks?book.imageLinks.smallThumbnail:null})` }}></div>
-                <ShelfChange
-                getCurrentBookStatus={getCurrentBookStatus}
-                handleStatusChange={handleStatusChange}
-                currentBook={book}
-                bookShelfTypes={bookShelfTypes}/>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">
-                    {book.authors?
-                    book.authors.reduce((acc, cur) => acc+","+cur):
-                    null
-                    }
-                </div>
+    return (
+        <div className="book">
+            <div className="book-top">
+            <div className="book-cover" 
+            style={{ width: 128, 
+                        height: 193, 
+                        backgroundImage: `url(${book.imageLinks?book.imageLinks.smallThumbnail:null})` }}></div>
+            <ShelfChange
+            getCurrentBookStatus={getCurrentBookStatus}
+            handleStatusChange={handleStatusChange}
+            currentBook={book}
+            bookShelfTypes={bookShelfTypes}/>
             </div>
-        );
-    }
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">
+                {book.authors?
+                book.authors.reduce((acc, cur) => acc+","+cur):
+                null
+                }
+            </div>
+        </div>
+    );
 }
 
 Book.propTypes = {
--- src/ListBook.js
+++ src/ListBook.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 import BookShelf from './BookShelf';
 import { Link } from 'react-router-dom';
 import { fetchKeyArrayfromDict, isShelfTypeIgnored } from './utils';
 
-class ListBook extends Component {
-    render(){
-        const { bookShelfTypes, books, handleStatusChange} = this.props;
-        const keyArr = fetchKeyArrayfromDict(bookShelfTypes);
-        return(
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                    {
-                        keyArr
-                        .filter(bookShelf => !isShelfTypeIgnored(bookShelf))
-                        .map((bookShelf, index) => (
-                            <BookShelf
-                                key={index}
-                                shelfTitle={bookShelfTypes[bookShelf]}
-                                books={books.filter(book => book.shelf === bookShelf)}
-                                handleStatusChange={handleStatusChange}
-                                bookShelfTypes={bookShelfTypes}
-                            />
-                        ))
-                    }
-                    </div>
-                    <Link to='/search'>
-                        <div className="open-search">
-                            <button className="open-search">
-                                Add a book
-                            </button>
-                        </div>
-                    </Link>
-                </div>
+const ListBook = props => {
+    const { bookShelfTypes, books, handleStatusChange} = props;
+    const keyArr = fetchKeyArrayfromDict(bookShelfTypes);
+    return(
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
             </div>
-          );
-    }
+            <div className="list-books-content">
+                <div>
+                {
+                    keyArr
+                    .filter(bookShelf => !isShelfTypeIgnored(bookShelf))
+                    .map((bookShelf, index) => (
+                        <BookShelf
+                            key={index}
+                            shelfTitle={bookShelfTypes[bookShelf]}
+                            books={books.filter(book => book.shelf === bookShelf)}
+                            handleStatusChange={handleStatusChange}
+                            bookShelfTypes={bookShelfTypes}
+                        />
+                    ))
+                }
+                </div>
+                <Link to='/search'>
+                    <div className="open-search">
+                        <button className="open-search">
+                            Add a book
+                        </button>
+                    </div>
+                </Link>
+            </div>
+        </div>
+        );
 }
 
 ListBook.propTypes = {
